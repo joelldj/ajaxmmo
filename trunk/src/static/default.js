41d2ff6735158881954410cf6382e7ac46f2dc67
jlebrech <jlebrech@b7651016-b0a5-11dd-98eb-e31df2311c8a>
2008-11-20 15:12:11 +0000

    jsonlint-ified

--- trunk/src/static/default.js
+++ trunk/src/static/default.js
@@ -1,54 +1,54 @@
-function placeTiles(json){
-	var tilesize = 32;
-	
-	alert("hi here");
-	
-    $.each(json.tiles, function(i,data){
-    	
-    	tilecolor = "rgb(" + this.alt + "," + this.alt + "," + this.alt + ")";
-
-                $("<div class='tile'>").appendTo("body")
-        .html(this.id)
-        .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px"})
-        .attr("id", this.id).click( function(){
-        $.post("/click?id=" + this.id);
-        });
-    });
-}
-
-function placeUnits(json){
-	
-	var tilesize = 32;
-	
-        $.each(json.units, function(i,data){
-                if ($(this.id).is("*")){
-                    $(this.id).html(this.id)
-            .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px"})
-            .attr("id", this.id).click( function(){
-            $.post("/click?id=" + this.id);
-        });
-
-            } else {
-                    $("<div class='unit'>").appendTo("body")
-            .html(this.id)
-            .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px"})
-            .attr("id", this.id).click( function(){
-            $.post("/click?id=" + this.id);
-            });
-                }
-    });
-}
+var tilesize = 32;
    
+function placeTiles(json){
+        $.each(json.tiles, function(i,data){
+                /* if $(this.id) exists then access it with $("#id"). then change if required */
+                /* tiles should not have not refresh as much as Units */
+               
+                tilecolor = "rgb(" + this.alt + "," + this.alt + "," + this.alt + ")";
+               
+                        if ($("x" + this.x + "y" + this.y).is("*")){
+                                $("x" + this.x + "y" + this.y).html("")
+                        .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px",backgroundColor: tilecolor})
+                        .attr("id", "x" + this.x + "y" + this.y).click( function(){
+                        $.post("/click?id=" + this.id);
+                    });
+                                /* a click event should be bound to the Tile to pass cursor location to background process */
+                        } else {
+                                $("<div class='tile'>").appendTo("body")
+                        .html("")
+                        .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px",backgroundColor: tilecolor})
+                        .attr("id", "x" + this.x + "y" + this.y).click( function(){ // id == x#y#
+                        $.post("/click?id=" + this.id);
+                    });
+                        }
+    });
+    }
+   
+    function placeUnits(json){
+                $.each(json.units, function(i,data){
+                        /* if $(this.id) exists then access it with $("#id"). then change if required */
+                        if ($(this.id).is("*")){
+                                $(this.id).html(this.id)
+                        .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px"})
+                        .attr("id", this.id).click( function(){
+                        $.post("/click?id=" + this.id);
+                    });
+                                /* click event should be bound to the Unit to send to the background process */
+                        } else {
+                                $("<div class='tile'>").appendTo("body")
+                        .html(this.id)
+                        .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px"})
+                        .attr("id", this.id).click( function(){
+                        $.post("/click?id=" + this.id);
+                    });
+                        }
+        });
+    }
+
 
 $(document).ready(function(){
-	$.getJSON('/tile',function(data){
-		alert("hi");
-	});
-	
-	$.getJSON('/tile', 
-    		placeTiles 
-    );
-    
-    $.getJSON('/unit', placeUnits );
+    $.getJSON('/tile', placeTiles );
+    $.getJSON('/unit', placeUnits ); 
 });
 
