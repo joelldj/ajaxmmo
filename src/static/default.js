c177c2ead28685bb1026b7fef4108f8d2e210e92
jlebrech <jlebrech@b7651016-b0a5-11dd-98eb-e31df2311c8a>
2008-12-05 14:47:02 +0000

    user interaction, page refreshing.
    loading units and tiles.
    only drawing new stuff.

--- src/static/default.js
+++ src/static/default.js
@@ -2,18 +2,15 @@ var tilesize = 32;
    
 function placeTiles(json){
         $.each(json.tiles, function(i,data){
-                /* if $(this.id) exists then access it with $("#id"). then change if required */
-                /* tiles should not have not refresh as much as Units */
-               
-        //tilecolor = "rgb(" + this.alt + "," + this.alt + "," + this.alt + ")";
-        	
-        //tilecolor = "rgb(0,250,0)";
-               
-		if ($(this.x + "-" + this.y).is("*")){
+        /* if $(this.id) exists then access it with $("#id"). then change if required */
+        /* tiles should not have not refresh as much as Units */
+              
+        if ($("#tile" + this.x + "-" + this.y).length > 0){	
 		        $(this.x + "-" + this.y).html(this.x + ":" + this.y)
 		        .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px",backgroundColor: tilecolor, zIndex: "1"})
 		        .attr("id", this.x + "-" + this.y).click( function(){
-		        	$.getJSON("/click?id=" + this.id, placeTiles );
+		        	// clicking on a tile we repeat the cycle of loading units then retriving what they can see
+		        	$.getJSON("/click?id=" + this.id, placeUnits );
 		        });
 		        /* a click event should be bound to the Tile to pass cursor location to background process */
 		} else {
@@ -21,8 +18,9 @@ function placeTiles(json){
 				.addClass(this.alt) /* class comes from json, then the tile has two classes such as .tile and .grass */
 		        .html(this.x + ":" + this.y)
 		        .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px", zIndex: "1"})
-		        .attr("id", this.x + "-" + this.y).click( function(){ // id == x#y#
-		        	$.getJSON("/click?id=" + this.id, placeTiles );
+		        .attr("id", "tile" + this.x + "-" + this.y).click( function(){ // id = x-y
+		        	// clicking on a tile we repeat the cycle of loading units then retriving what they can see
+		        	$.getJSON("/click?id=" + this.id, placeUnits );
 		        });
 		}
     });
@@ -34,10 +32,10 @@ function placeTiles(json){
                     	$.getJSON('/tile?id=' + this.id, placeTiles );
                 	
                         /* if $(this.id) exists then access it with $("#id"). then change if required */
-                        if ($("u" + this.id).is("*")){
-                                $("u" + this.id).html(this.x + ":" + this.y)
+                        if ($("#unit" + this.id).length > 0){
+                                $(this.id).html(this.x + ":" + this.y)
                                 .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px", zIndex: "2"})
-                                .attr("id", this.id).click( function(){
+                                .attr("id", "unit" + this.id).click( function(){
                                 	$.getJSON("/click?id=" + this.id, placeUnits ); 
                                 });
                                 /* click event should be bound to the Unit to send to the background process */
@@ -45,7 +43,7 @@ function placeTiles(json){
                                 $("<div class='unit'>").appendTo("body")
                                 .html(this.x + ":" + this.y)
                                 .css({position: "absolute", left: this.x*tilesize+"px", top: this.y*tilesize+"px", zIndex: "2", backgroundColor: "red"})
-                                .attr("id", this.id).click( function(){
+                                .attr("id", "unit" + this.id).click( function(){
                                 	$.getJSON("/click?id=" + this.id, placeUnits ); 
                                 });
                         }
