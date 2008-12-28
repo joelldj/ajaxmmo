c65e3002c1d07d941b2e9ed114b3adb2a9c41509
jlebrech <jlebrech@b7651016-b0a5-11dd-98eb-e31df2311c8a>
2008-12-28 21:12:23 +0000

    smaller pic :)

--- src/mainpage.py
+++ src/mainpage.py
@@ -47,7 +47,8 @@ class GetTiles(webapp.RequestHandler):
 
     json = {"tiles":[]}
          
-    pngfile = urlfetch.fetch(url="http://humanitymmo.appspot.com/static/earth.png").content
+    pngfile = 
+urlfetch.fetch(url="http://humanitymmo.appspot.com/static/earthmini.png").content
     
     reader = png.Reader(file=StringIO(pngfile)) # streams are also accepted
     w, h, pixels, metadata = reader.read()
