09e1ee398cc6a706b828c98a84009042d1694244
jlebrech <jlebrech@b7651016-b0a5-11dd-98eb-e31df2311c8a>
2009-01-15 21:51:56 +0000

    chat update

--- trunk/src/mainpage.py
+++ trunk/src/mainpage.py
@@ -28,7 +28,7 @@ class MainPage(webapp.RequestHandler):
     head = head + """<script src="/static/default.js" type="text/javascript"></script>"""
     head = head + """<link href="/static/default.css" rel="stylesheet" type="text/css" />"""
     head = head + """<link href="/static/tiles.css" rel="stylesheet" type="text/css" />"""
-    head = head + """</head><body><div id="world" style="position:relative" > <span id="cursor" /> </div> <div id="chatroom" />"""
+    head = head + """</head><body><div id="world" style="position:relative" > <span id="cursor" /> </div> <div id="chatroom" /> <input type=text id="msgbox" />"""
     
     self.response.out.write(head)
     
