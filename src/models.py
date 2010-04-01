32b170f41a17d1789dbe3f08e89f7cbc12795355
jlebrech <jlebrech@b7651016-b0a5-11dd-98eb-e31df2311c8a>
2010-04-01 11:10:17 +0000

    model tweak, #todo logic.

--- src/models.py
+++ src/models.py
@@ -1,16 +1,33 @@
 from google.appengine.ext import db
 from google.appengine.api import users
 
+class TileType(db.Model):
+    name = db.StringProperty()
+    canTravel = db.BooleanProperty()
+
 class Tile(db.Model):
     x = db.IntegerProperty()
     y = db.IntegerProperty()
     height = db.IntegerProperty()
     type = db.IntegerProperty()
 
+class Faction(db.Model): # spain, france...
+    name = db.StringProperty()
+
+class Dock(db.Model):
+    location = db.ReferenceProperty(Tile)
+    faction = db.ReferenceProperty(Faction) # ie. ships must be neutral
+
+class AggresivityType(db.Model):
+    name = db.StringProperty()
+
 class Unit(db.Model):
     x = db.IntegerProperty()
     y = db.IntegerProperty()
     user = db.UserProperty()
+    faction = db.ReferenceProperty(Faction) # spain, france...
+    dock = db.ReferenceProperty(Dock) # can be none
+    aggresivity = db.ReferenceProperty(AggresivityType)
 
 class ChatMessage(db.Model):
     text = db.StringProperty()
